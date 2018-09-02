2f4dfd74b48d28ddc9196cd01a79f10ccd843be8
Jack Bendtsen <jack@dimon.com.au>
2018-09-02 12:28:13 +1000

    Initial design

---
+++ readme.md
@@ -0,0 +1,8 @@
+# libsubzero
+A (tiny) library for modifying .srt subtitle files.
+Features include:
+- Offsetting all timestamps by a certain time interval (+/-)
+- Editing specific timestamps
+- Search subtitles by text or by time
+- Retrieve/insert/delete subtitle
+- Retrieve/insert/delete line of text
---
+++ srt.h
@@ -0,0 +1,39 @@
+#ifndef SRT_H
+#define SRT_H
+
+typedef struct {
+	int start_time;
+	int end_time;
+	char *lines;
+	int n_lines;
+} sub_t;
+
+typedef struct {
+	sub_t *sub;
+	int n_subs;
+	int enc;
+} srt_t;
+
+char *subzero_time_string(int stamp);
+int subzero_time_int(char *stamp);
+
+int subzero_parse_srt(srt_t *srt, char *buf);
+char *subzero_write_srt(srt_t *srt);
+
+int subzero_set_encoding(srt_t *srt, int enc);
+
+int subzero_get_subtitle(srt_t *srt, sub_t *sub, int idx);
+int subzero_insert_subtitle(srt_t *srt, sub_t *sub, int idx);
+int subzero_delete_subtitle(srt_t *srt, int idx);
+
+int subzero_get_line(sub_t *sub, int idx);
+int subzero_get_text(sub_t *sub);
+int subzero_insert_lines(sub_t *sub, int idx, char *text);
+int subzero_delete_line(sub_t *sub, int idx);
+
+int subzero_search_by_text(srt_t *srt, char *text, int *res);
+int subzero_search_by_time(srt_t *srt, int start, int end, int *res);
+
+int subzero_offset_timestamps(srt_t *srt, int val);
+
+#endif
